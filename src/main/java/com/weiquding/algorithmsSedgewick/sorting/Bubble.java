35a2af42bb971732a7adbb09458fdac3c3c4be6e
peter <believeyourself@outlook.com>
2019-09-20 15:37:17 +0800

    commit insertion

--- src/main/java/com/weiquding/algorithmsSedgewick/sorting/Bubble.java
+++ src/main/java/com/weiquding/algorithmsSedgewick/sorting/Bubble.java
@@ -4,44 +4,21 @@ import com.weiquding.stdlib.In;
 import com.weiquding.stdlib.StdOut;
 
 /**
- * 选择排序：不断地选择剩余元素中的最小元素
- * 1.实现
- * 1.1 找到数组中最小的那个最小的元素，将它与数组的第一个元素交换位置。
- * 1.2 在剩下的元素中找到最小的元素，将它与数组的第二个元素交换位置。
- * 1.3 如此往复，直到整个数组元素排序。
- * <p>
- * 2. 特点：
- * 2.1 运行时间与输入无关: 输入数据有序或无序都不影响比较次数
- * 2.2 数据移动是最少的：每次交换都会改变两个数组元素的值，因此选择排序用了N次交换。
- * <p>
- * 3. 复杂度分析：
- * 3.1 时间复杂度： O(N^2)
- * 3.2 空间复杂度： O(1)
- * <p>
- * 4 稳定性（如果一个排序算法能够保留数组中重复元素的相对位置则可以被称为是稳定的）：稳定
+ * 冒泡:
+ * 1. 把大的元素往向后调，比较相邻的两个元素，交换发生在两个元素之间，如两个元素相等则不会交换，稳定。
  */
+public class Bubble {
 
-public class Selection {
-
-    /**
-     * 将a[]进行升序排列
-     *
-     * @param a
-     */
     public static void sort(Comparable[] a) {
-        // 具体算法实现 [选择]
+        // 冒泡排序
         int length = a.length;
-        for (int i = 0; i < length; i++) {
-            // 将a[i]与a[i+1... length-1]中最小元素交换
-            int min = i;
-            for (int j = i + 1; j < length; j++) {
-                if(less(a[j], a[min])){
-                    min = j;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (less(a[j + 1], a[j])) {
+                    exch(a, j + 1, j);
                 }
             }
-            exch(a, i, min);
         }
-
     }
 
     /**
